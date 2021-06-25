8a132b23284b3fc40007fe8b09ca82144d86a144
mauleyzaola <mauricio.leyzaola@conekta.com>
2021-06-25 07:02:42 -0500

    updates

--- main.go
+++ main.go
@@ -12,38 +12,40 @@ var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func main() {
 	wg := &sync.WaitGroup{}
-	// introduced this for/loop to show how we are not really caching database
-	for j := 0; j < 10; j++ {
-		for i := 0; i < 10; i++ {
-			id := rnd.Intn(10) + 1
-			wg.Add(2)
-			go func(id int, wg *sync.WaitGroup) {
-				if b, ok := queryCache(id); ok {
-					fmt.Println("from cache")
-					fmt.Println(b)
-				}
-				wg.Done()
-			}(id, wg)
-			go func(id int, wg *sync.WaitGroup) {
-				if b, ok := queryDatabase(id); ok {
-					fmt.Println("from database")
-					fmt.Println(b)
-				}
-				wg.Done()
-			}(id, wg)
-			time.Sleep(time.Millisecond * 150)
-		}
+	m := &sync.Mutex{}
+	for i := 0; i < 10; i++ {
+		id := rnd.Intn(10) + 1
+		wg.Add(2)
+		go func(id int, wg *sync.WaitGroup, mutex *sync.Mutex) {
+			if b, ok := queryCache(id, m); ok {
+				fmt.Println("from cache")
+				fmt.Println(b)
+			}
+			wg.Done()
+		}(id, wg, m)
+		go func(id int, wg *sync.WaitGroup, mutex *sync.Mutex) {
+			if b, ok := queryDatabase(id, m); ok {
+				fmt.Println("from database")
+				fmt.Println(b)
+			}
+			wg.Done()
+		}(id, wg, m)
+		time.Sleep(time.Millisecond * 150)
 	}
 	// wait exactly for all wait groups to complete
 	wg.Wait()
 }
 
-func queryCache(id int) (Book, bool) {
+func queryCache(id int, m *sync.Mutex) (Book, bool) {
+	m.Lock()
+	defer m.Unlock()
 	b, ok := cache[id]
 	return b, ok
 }
 
-func queryDatabase(id int) (Book, bool) {
+func queryDatabase(id int, m *sync.Mutex) (Book, bool) {
+	m.Lock()
+	defer m.Unlock()
 	time.Sleep(time.Millisecond * 100)
 	for _, b := range books {
 		if b.ID == id {
