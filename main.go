b1d430195328b1fc668bf34fb0793a4b8c8c1508
mauleyzaola <mauricio.leyzaola@conekta.com>
2021-06-25 07:01:02 -0500

    updates

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -10,24 +11,31 @@ var cache = map[int]Book{}
 var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func main() {
-	for i := 0; i < 10; i++ {
-		id := rnd.Intn(10) + 1
-		go func(id int) {
-			if b, ok := queryCache(id); ok {
-				fmt.Println("from cache")
-				fmt.Println(b)
-			}
-		}(id)
-		go func(id int) {
-			if b, ok := queryDatabase(id); ok {
-				fmt.Println("from database")
-				fmt.Println(b)
-			}
-		}(id)
-		time.Sleep(time.Millisecond * 150)
+	wg := &sync.WaitGroup{}
+	// introduced this for/loop to show how we are not really caching database
+	for j := 0; j < 10; j++ {
+		for i := 0; i < 10; i++ {
+			id := rnd.Intn(10) + 1
+			wg.Add(2)
+			go func(id int, wg *sync.WaitGroup) {
+				if b, ok := queryCache(id); ok {
+					fmt.Println("from cache")
+					fmt.Println(b)
+				}
+				wg.Done()
+			}(id, wg)
+			go func(id int, wg *sync.WaitGroup) {
+				if b, ok := queryDatabase(id); ok {
+					fmt.Println("from database")
+					fmt.Println(b)
+				}
+				wg.Done()
+			}(id, wg)
+			time.Sleep(time.Millisecond * 150)
+		}
 	}
-	// leave some time for all go routines to complete execution
-	time.Sleep(time.Second * 2)
+	// wait exactly for all wait groups to complete
+	wg.Wait()
 }
 
 func queryCache(id int) (Book, bool) {
@@ -39,6 +47,7 @@ func queryDatabase(id int) (Book, bool) {
 	time.Sleep(time.Millisecond * 100)
 	for _, b := range books {
 		if b.ID == id {
+			// now we have collisions between goroutines accessing the same shared memory
 			cache[id] = b
 			return b, true
 		}
